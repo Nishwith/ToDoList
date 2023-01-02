dc0ae22f806c5e70592e6c04707051ccd71216fe
Nishwith <nishwithache@gmail.com>
2023-01-02 20:01:56 +0530

    Initial commit

--- app.js
+++ app.js
@@ -110,6 +110,6 @@ app.get("/:customListsName", function(req, res) {
     })
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 5000, function() {
     console.log("Server started on port 3000");
 });
